2fcef7f93a1be185028e18bc14fc42fa769875b3
claxten10 <claxten10@gmail.com>
2025-06-22 13:07:22 +0000

    ims: Copy missing AOSP IMS permission
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- ims.mk
+++ ims.mk
@@ -39,4 +39,5 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
